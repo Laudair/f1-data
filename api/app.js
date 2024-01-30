6120b2ec4bf73a3bda523b47faa09540af8152b4
Laudair <47931648+Laudair@users.noreply.github.com>
2024-01-30 08:33:12 +0000

    update api script to get session

--- api/app.js
+++ api/app.js
@@ -41,6 +41,41 @@ app.get('/get-f1-data', (req, res) => {
     });
 });
 
+app.get('/get-f1-session', (req, res) => {
+    const { year, race } = req.query;
+
+    const ref = db.ref(`f1data/${year}_${race}`);
+    ref.once('value', (snapshot) => {
+
+        if (snapshot.exists()) {
+            console.log('Data found in Firebase. Sending data...');
+            res.json(snapshot.val());
+        } else {
+            console.log('Data not found in Firebase. Running Python script to get sesion...');
+            const getSessionCommand = `python ../scripts/session_results.py ${year} ${race}`;
+            exec(getSessionCommand, (error, stdout, stderr) => {
+                if(error) {
+                    console.error(`Exec error: ${error}`);
+                    return res.status(500).send(stderr);
+                }
+                console.log('Python script executed. Fetching new data from Firebase to get sessions...');
+                ref.once('value', (newSnapshot) => {
+                    if (newSnapshot.exists()) {
+                        res.json(newSnapshot.val());
+                    } else {
+                        res.status(500).send('Error fetching new data');
+                    }
+                });
+            })
+        }
+
+    }, (error) => {
+        console.error('Firebase read error:', error);
+        res.status(500).send('Error fetching data from Firebase');
+    })
+
+});
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
